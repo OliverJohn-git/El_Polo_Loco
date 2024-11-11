8ddbacf0240b627aba681a9a4f81873b624f8096
OliverJohn-git <johnoliver846@gmail.com>
2024-11-11 10:38:45 +0100

    chicken and Clouds in canvas and randomly placed

--- models/world.class.js
+++ models/world.class.js
@@ -5,13 +5,36 @@ class World {
             new Chicken(),
             new Chicken(),
     ];
+    clouds = [
+        new Cloud()
+    ]
+    canvas;
+    ctx;
 
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas
         this.draw();
     };
 
     draw(){
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y,this.character.height, this.character.width );
+        
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //clears canvas
+
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.height, this.character.width );
+
+        this.enemies.array.forEach(enemy => {
+            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.height, enemy.width );
+        });
+
+        this.clouds.array.forEach(cloud => {
+            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.height, cloud.width );
+        });
+
+        //repeating draw as much as possible for users GPU
+        let self = this;
+        requestAnimationFrame(function() {
+            self.draw()
+        });
     };
 }
