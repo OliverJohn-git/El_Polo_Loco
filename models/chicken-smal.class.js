2b24931e8c18bab9135b4c9095c16007371d6ba1
OliverJohn-git <johnoliver846@gmail.com>
2024-11-15 10:24:52 +0100

    added all img´s for Character, Chicken´s and Boss to the code

--- models/chicken-smal.class.js
+++ models/chicken-smal.class.js
@@ -5,19 +5,19 @@ class Chicken extends MovableObject{
     path;
 
     IMAGES_WALKING = [
-        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
-        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
-        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_small/1_walk/3_w.png',
     ];
 
     IMAGES_DEAD = [
-       'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+       'img/3_enemies_chicken/chicken_small/2_dead/dead.png'
     ];
 
     currentImage = 0;
     
     constructor() {
-        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+        super().loadImage('img/3_enemies_chicken/chicken_small/1_walk/1_w.png')
         this.x = 250 + Math.random() * 500 ;
         this.loadImages(this.IMAGES_WALKING);
         this.speed = 0.15 + Math.random() * 0.5
--- models/endboss.class.js
+++ models/endboss.class.js
@@ -9,14 +9,40 @@ class Endboss extends MovableObject{
         'img/4_enemie_boss_chicken/1_walk/G2.png',
         'img/4_enemie_boss_chicken/1_walk/G3.png',
         'img/4_enemie_boss_chicken/1_walk/G4.png',
-        'img/4_enemie_boss_chicken/1_walk/G5.png',
-        'img/4_enemie_boss_chicken/1_walk/G6.png',
-        'img/4_enemie_boss_chicken/1_walk/G7.png',
-        'img/4_enemie_boss_chicken/1_walk/G8.png',
-        'img/4_enemie_boss_chicken/1_walk/G9.png',
-        'img/4_enemie_boss_chicken/1_walk/G10.png',
-        'img/4_enemie_boss_chicken/1_walk/G11.png',
-        'img/4_enemie_boss_chicken/1_walk/G12.png',
+    ];
+
+    IMAGES_ALERT = [
+        'img/4_enemie_boss_chicken/2_alert/G5.png',
+        'img/4_enemie_boss_chicken/2_alert/G6.png',
+        'img/4_enemie_boss_chicken/2_alert/G7.png',
+        'img/4_enemie_boss_chicken/2_alert/G8.png',
+        'img/4_enemie_boss_chicken/2_alert/G9.png',
+        'img/4_enemie_boss_chicken/2_alert/G10.png',
+        'img/4_enemie_boss_chicken/2_alert/G11.png',
+        'img/4_enemie_boss_chicken/2_alert/G12.png',
+    ];
+
+    IMAGES_ATTACK = [
+        'img/4_enemie_boss_chicken/3_attack/G13.png',
+        'img/4_enemie_boss_chicken/3_attack/G14.png',
+        'img/4_enemie_boss_chicken/3_attack/G15.png',
+        'img/4_enemie_boss_chicken/3_attack/G16.png',
+        'img/4_enemie_boss_chicken/3_attack/G17.png',
+        'img/4_enemie_boss_chicken/3_attack/G18.png',
+        'img/4_enemie_boss_chicken/3_attack/G19.png',
+        'img/4_enemie_boss_chicken/3_attack/G20.png',
+    ];
+
+    IMAGES_HURT = [
+        'img/4_enemie_boss_chicken/4_hurt/G21.png',
+        'img/4_enemie_boss_chicken/4_hurt/G22.png',
+        'img/4_enemie_boss_chicken/4_hurt/G23.png',
+    ];
+
+    IMAGES_DEAD = [
+        'img/4_enemie_boss_chicken/5_dead/G24.png',
+        'img/4_enemie_boss_chicken/5_dead/G25.png',
+        'img/4_enemie_boss_chicken/5_dead/G26.png',
     ];
 
     currentImage = 0;
@@ -30,9 +56,9 @@ class Endboss extends MovableObject{
 
     animate(){
 
-        this.playAnimation(this.IMAGES_WALKING);
-        // setInterval(() => {
-        //     this.playAnimation(this.IMAGES_WALKING);
-        // }, 1000/25)
+        // this.playAnimation(this.IMAGES_WALKING);
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALKING);
+        }, 1000/25)
     }
 }
