04db73157a1edabaa3c00534254c9ba414f3a6ec
OliverJohn-git <johnoliver846@gmail.com>
2024-11-21 03:06:40 +0100

    statusbars fix

--- models/status-bar.class.js
+++ models/status-bar.class.js
@@ -3,28 +3,28 @@ class StatusBar extends DrawableObject{
 
     IMAGES = {
         "health":[
-      "assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png",
-      "assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png",
-      "assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png",
-      "assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png",
-      "assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png",
-      "assets/img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png",
+      "img/7_statusbars/1_statusbar/2_statusbar_health/green/0.png",
+      "img/7_statusbars/1_statusbar/2_statusbar_health/green/20.png",
+      "img/7_statusbars/1_statusbar/2_statusbar_health/green/40.png",
+      "img/7_statusbars/1_statusbar/2_statusbar_health/green/60.png",
+      "img/7_statusbars/1_statusbar/2_statusbar_health/green/80.png",
+      "img/7_statusbars/1_statusbar/2_statusbar_health/green/100.png",
     ],
     "coins":[
-      'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/green/0.png',
-      'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/green/20.png',
-      'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/green/40.png',
-      'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/green/60.png',
-      'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/green/80.png',
-      'assets/img/7_statusbars/1_statusbar/1_statusbar_coin/green/100.png',
+      'img/7_statusbars/1_statusbar/1_statusbar_coin/green/0.png',
+      'img/7_statusbars/1_statusbar/1_statusbar_coin/green/20.png',
+      'img/7_statusbars/1_statusbar/1_statusbar_coin/green/40.png',
+      'img/7_statusbars/1_statusbar/1_statusbar_coin/green/60.png',
+      'img/7_statusbars/1_statusbar/1_statusbar_coin/green/80.png',
+      'img/7_statusbars/1_statusbar/1_statusbar_coin/green/100.png',
     ],
     "bottles":[
-      'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/0.png',
-      'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/20.png',
-      'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/40.png',
-      'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/60.png',
-      'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/80.png',
-      'assets/img/7_statusbars/1_statusbar/3_statusbar_bottle/green/100.png',
+      'img/7_statusbars/1_statusbar/3_statusbar_bottle/green/0.png',
+      'img/7_statusbars/1_statusbar/3_statusbar_bottle/green/20.png',
+      'img/7_statusbars/1_statusbar/3_statusbar_bottle/green/40.png',
+      'img/7_statusbars/1_statusbar/3_statusbar_bottle/green/60.png',
+      'img/7_statusbars/1_statusbar/3_statusbar_bottle/green/80.png',
+      'img/7_statusbars/1_statusbar/3_statusbar_bottle/green/100.png',
     ]
 };
     selectedBar;
